8e4d2eb7e7c2b811baf50e9ab55e9a698ddaf3a3
Usman Gurin <101094206+usmangurin@users.noreply.github.com>
2022-09-09 23:22:28 +0100

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	Return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: Print with proper grammar, but the outcome is a piece of art.
+ *
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+     Printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0)
+}
